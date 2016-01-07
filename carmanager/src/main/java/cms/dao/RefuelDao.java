4e76e0f747735ec8568c1883535558ecea617c93
JaeHan-Kim <hgal8877@gmail.com>
2016-01-07 16:06:23 +0900

    refuel추가

---
+++ carmanager/src/main/java/cms/dao/RefuelDao.java
@@ -0,0 +1,9 @@
+package cms.dao;
+
+import java.util.List;
+
+import cms.domain.Refuel;
+
+public interface RefuelDao {
+	public List<Refuel> selectRefuelList(int no);
+}
--- carmanager/src/main/java/cms/service/support/DefaultRefuelService.java
+++ carmanager/src/main/java/cms/service/support/DefaultRefuelService.java
@@ -5,17 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cms.dao.RefuelDao;
+import cms.dao.RefuelDao;
 import cms.domain.Refuel;
 import cms.service.RefuelService;
 
 @Service
 public class DefaultRefuelService implements RefuelService {
-  @Autowired RefuelDao refuelDao;
+	@Autowired RefuelDao refuelDao;
 
-  @Override
-  public List<Refuel> listAll(int no) {
-    return refuelDao.selectRefuelList(no);
-  }
- 
+	@Override
+	public List<Refuel> listAll(int no) {
+		return refuelDao.selectRefuelList(no);
+	}
+	
+	
 }
